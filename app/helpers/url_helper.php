b8f29d97786564783f3ae5d571428818299cfe14
Massy100 <massielle.coti@gmail.com>
2023-12-15 17:26:53 -0600

    ya funciona el register

--- app/helpers/url_helper.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function redirection($url){
-    $url = filter_var($url, FILTER_SANITIZE_URL);
-    header("location:" . URL_PROJECT . $url);
-    exit();
-}
--- public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Llamando al iniciador
-require_once '../app/initializer.php';
-
-// Iniciando al core
-$init = new Core;
